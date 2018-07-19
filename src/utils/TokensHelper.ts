6c7fd1bce31ea26848e7558e54e18cc856a680df
SprinterGameDev <sprinter@ozersk.com>
2018-07-19 15:32:13 +0500

    imports of icon for coins. !if icon not found then return undefined!

--- src/utils/TokensHelper.ts
+++ src/utils/TokensHelper.ts
@@ -1,13 +1,46 @@
+import ada from '../res/icons/tokens/ada.svg';
 import ae from '../res/icons/tokens/ae.svg';
+import ardr from '../res/icons/tokens/ardr.svg';
+import bat from '../res/icons/tokens/bat.svg';
+import bcd from '../res/icons/tokens/bcd.svg';
+import bcn from '../res/icons/tokens/bcn.svg';
 import bnb from '../res/icons/tokens/bnb.svg';
 import btc from '../res/icons/tokens/btc.svg';
+import btg from '../res/icons/tokens/btg.svg';
+import bts from '../res/icons/tokens/bts.svg';
+import dash from '../res/icons/tokens/dash.svg';
 import eos from '../res/icons/tokens/eos.svg';
+import etc from '../res/icons/tokens/etc.svg';
 import eth from '../res/icons/tokens/eth.svg';
+import gnt from '../res/icons/tokens/gnt.svg';
+import hsr from '../res/icons/tokens/hsr.svg';
 import icx from '../res/icons/tokens/icx.svg';
+import iost from '../res/icons/tokens/iost.svg';
+import lsk from '../res/icons/tokens/lsk.svg';
+import ltc from '../res/icons/tokens/ltc.svg';
+import nano from '../res/icons/tokens/nano.svg';
+import neo from '../res/icons/tokens/neo.svg';
+import npxs from '../res/icons/tokens/npxs.svg';
 import omg from '../res/icons/tokens/omg.svg';
+import ont from '../res/icons/tokens/ont.svg';
+import ppt from '../res/icons/tokens/ppt.svg';
+import qtum from '../res/icons/tokens/qtum.svg';
+import rep from '../res/icons/tokens/rep.svg';
+import sc from '../res/icons/tokens/sc.svg';
+import snt from '../res/icons/tokens/snt.svg';
+import steem from '../res/icons/tokens/steem.svg';
+import strat from '../res/icons/tokens/strat.svg';
 import trx from '../res/icons/tokens/trx.svg';
 import usdt from '../res/icons/tokens/usdt.svg';
 import ven from '../res/icons/tokens/ven.svg';
+import wan from '../res/icons/tokens/wan.svg';
+import waves from '../res/icons/tokens/waves.svg';
+import wtc from '../res/icons/tokens/wtc.svg';
+import xem from '../res/icons/tokens/xem.svg';
+import xlm from '../res/icons/tokens/xlm.svg';
+import xmr from '../res/icons/tokens/xmr.svg';
+import xvg from '../res/icons/tokens/xvg.svg';
+import zec from '../res/icons/tokens/zec.svg';
 import zil from '../res/icons/tokens/zil.svg';
 import zrx from '../res/icons/tokens/zrx.svg';
 
@@ -20,11 +53,17 @@ export class TokensHelper {
 
   private static readonly ICONS: Map<string, any> = new Map([
     ['USDT', usdt], ['Bitcoin', btc], ['Eth', eth], ['EOS', eos], ['Tron', trx], ['VeChain', ven],
-    ['Binance', bnb], ['OmiseGO', omg], ['Icon', icx], ['Zilliqa', zil], ['Aeternity', ae], ['0x', zrx]
+    ['Binance', bnb], ['OmiseGO', omg], ['Icon', icx], ['Zilliqa', zil], ['Aeternity', ae], ['0x', zrx],
+    ['Populous', ppt], ['Ardor', ardr], ['Hshare', hsr], ['IOST', iost], ['Monero', xmr], ['Dash', dash],
+    ['Eth Classic', etc], ['NEM', xem], ['Zcash', zec], ['Qtum', qtum], ['Bytecoin', bcn], ['Lisk', lsk],
+    ['BitShares', bts], ['Ontology', ont], ['BTC Gold', btg], ['Siacoin', sc], ['Steem', steem],
+    ['Verge', xvg], ['Nano', nano], ['BAT', bat], ['Augur', rep], ['BTC Diamond', bcd], ['Golem', gnt],
+    ['Pundi X', npxs], ['Stratis', strat], ['Waves', waves], ['Waltonchain', wtc], ['Status', snt],
+    ['Wanchain', wan], ['Stellar', xlm], ['NEO', neo], ['Litecoin', ltc], ['Cardano', ada]
   ]);
 
-  public static getIcon(name: string): any {
-    return TokensHelper.ICONS.get(name) || usdt;
+  public static getIcon(name: string): any | undefined {
+    return TokensHelper.ICONS.get(name) || undefined;
   }
 
   public static getColor(index: number): string {
