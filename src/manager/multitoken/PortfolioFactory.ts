a0d4f3023fccb6e3d8b03f355e18ab72584d5b21
SprinterGameDev <sprinter@ozersk.com>
2018-07-31 12:37:44 +0500

    portfolio manager factory

---
+++ src/manager/multitoken/PortfolioFactory.ts
@@ -0,0 +1,36 @@
+import Config from '../../Config';
+import { CryptocurrencyRepository } from '../../repository/cryptocurrency/CryptocurrencyRepository';
+import { CryptocurrencyTokensRepositoryImpl } from '../../repository/cryptocurrency/CryptocurrencyTokensRepositoryImpl';
+import { RebalanceHistory } from '../../repository/models/RebalanceHistory';
+import { ArbitrageursExecutor } from './executors/ArbitrageurExecutor';
+import { CapCalculatorExecutor } from './executors/CapCalculatorExecutor';
+import { ExchangerExecutorImpl } from './executors/ExchangerExecutorImpl';
+import { ManualRebalancerExecutorImpl } from './executors/ManualRebalancerExecutorImpl';
+import { TimeLineExecutor } from './executors/TimeLineExecutor';
+import { Multitoken } from './multitoken/Multitoken';
+import { MultitokenImpl } from './multitoken/MultitokenImpl';
+import { PortfolioManager } from './PortfolioManager';
+import PortfolioManagerImpl from './PortfolioManagerImpl';
+
+export class PortfolioFactory {
+
+  public static createDefaultPortfolio(): PortfolioManager {
+    const cryptocurrencyRepository: CryptocurrencyRepository =
+      new CryptocurrencyTokensRepositoryImpl(Config.getStatic());
+
+    const multitoken: Multitoken = new MultitokenImpl(RebalanceHistory.MULTITOKEN_NAME_REBALANCE);
+    const standardMultitoken: Multitoken = new MultitokenImpl(RebalanceHistory.MULTITOKEN_NAME_STANDARD);
+
+    const exchanger: TimeLineExecutor = new ExchangerExecutorImpl(multitoken, 10);
+    const arbitrageurs: TimeLineExecutor = new ArbitrageursExecutor(multitoken, 9);
+    const manualRebalancer: TimeLineExecutor = new ManualRebalancerExecutorImpl(multitoken, 8);
+    const capCalculator: TimeLineExecutor = new CapCalculatorExecutor([multitoken, standardMultitoken], 7);
+
+    return new PortfolioManagerImpl(
+      cryptocurrencyRepository,
+      [multitoken, standardMultitoken],
+      [exchanger, arbitrageurs, manualRebalancer, capCalculator]
+    );
+  }
+
+}
