80527491dff27c7e3f69e5dd2d2eb4442d5f4935
SprinterGameDev <sprinter@ozersk.com>
2018-07-31 14:01:41 +0500

    injection for MultiPortfolioExecutor

--- src/Injections.ts
+++ src/Injections.ts
@@ -5,11 +5,14 @@ import Config from './Config';
 import { AnalyticsManager } from './manager/analytics/AnalyticsManager';
 import { AnalyticsManagerImpl } from './manager/analytics/AnalyticsManagerImpl';
 import { GoogleAnalyticsImpl } from './manager/analytics/GoogleAnalitycsImpl';
+import { MultiPortfolioExecutor } from './manager/multitoken/MultiPortfolioExecutor';
+import { MultiPortfolioExecutorImpl } from './manager/multitoken/MultiPortfolioExecutorImpl';
 import { PortfolioFactory } from './manager/multitoken/PortfolioFactory';
 import { PortfolioManager } from './manager/multitoken/PortfolioManager';
 
 export enum Services {
   PORTFOLIO_MANAGER = 'PortfolioManager',
+  PORTFOLIOS_EXECUTOR = 'PortfoliosExecutor',
   ANALYTICS_MANAGER = 'AnalyticsManager'
 }
 
@@ -32,6 +35,9 @@ export {
 
 kernel.bind<PortfolioManager>(Services.PORTFOLIO_MANAGER)
   .toConstantValue(PortfolioFactory.createDefaultPortfolio());
+
+kernel.bind<MultiPortfolioExecutor>(Services.PORTFOLIOS_EXECUTOR)
+  .toConstantValue(new MultiPortfolioExecutorImpl());
 
 const googleAnalytics: GoogleAnalyticsImpl =
   new GoogleAnalyticsImpl(Config.getGoogleAnalyticsTrackId(), Config.isDebug());
